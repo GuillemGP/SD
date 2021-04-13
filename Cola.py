f99b9e00d8d1e4974c12f0d87a8fac997ef28b43
GuillemGP <76963941+GuillemGP@users.noreply.github.com>
2021-04-13 17:53:56 +0200

    Add files via upload

---
+++ Cola.py
@@ -0,0 +1,33 @@
+class Cola:
+    def __init__(self):
+        self.items = []
+
+    def encolar(self, x):
+        self.items.append(x)
+
+    def desencolar(self):
+        try:
+            return self.items.pop(0)
+        except:
+            raise ValueError("La cola esta vacia")
+
+    def es_vacia(self):
+        return self.items == []
+    
+    def print_cola(self):
+        for i in self.items:
+            print(i)
+
+#Main temporal de pruebas
+if __name__ == "__main__":
+    q = Cola()
+    q.es_vacia()
+
+    q.encolar(1)
+    q.encolar(2)
+    q.encolar(3)
+
+    q.print_cola()
+    q.desencolar()
+
+    q.es_vacia()
