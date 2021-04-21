0f5bf77ae8a5fbee7f0a1e378324c6b1fc69a3ee
GuillemGP <76963941+GuillemGP@users.noreply.github.com>
2021-04-21 18:59:31 +0200

    Add files via upload

--- Server/xmlrpc-server.py
+++ Server/xmlrpc-server.py
@@ -13,22 +13,22 @@ import time
 
 def WordCountServer(fichero):
     numberOfWorkers = nWorkers()
-    if numberOfWorkers > 0:
+    if numberOfWorkers > 0:                     #si tenemos workers creados (1 o mas)
         filesLength = len(fichero.split())
-        if filesLength==1:
+        if filesLength == 1:                    #si solo tenemos un fichero que tratar
             job = queue.enqueue(WordCount.contar_palabras,fichero)
             while job.result == None:
                 time.sleep(0.1)
             result = str(job.result) + "\n"
             return result
-        else:
+        else:                                   #si tenemos mas de un fichero para tratar                
             jobResults = []
             for file in fichero.split():
                 workerResult= []
                 job = queue.enqueue(WordCount.contar_palabras,file)
                 while job.result == None:
                     time.sleep(0.1)
-                workerResult.append(file)   #ponemos en el resultado del worker el nombre del fichero
+                workerResult.append(file)       #ponemos en el resultado del worker el nombre del fichero
                 workerResult.append(job.result) #ponemos en el resultado del worker el resultado obtenido
                 jobResults.append(workerResult) #añadimos este objeto a la lista
             job = queue.enqueue(putResultTogether.putResultTogether, args=(jobResults,"WordCount"))
@@ -36,28 +36,28 @@ def WordCountServer(fichero):
                 time.sleep(0.1)
             result = str(job.result) + "\n"
             return result
-    else:
+    else:                                       #si no tenemos ningun worker creado
         return("No hi ha cap worker actiu per encuar la tasca")
 
 def CountingWordsServer(fichero):
     numberOfWorkers = nWorkers()
-    if numberOfWorkers > 0:
+    if numberOfWorkers > 0:                     #si tenemos workers creados (1 o mas)
         result = ""
         filesLength = len(fichero.split())
-        if filesLength == 1:
+        if filesLength == 1:                    #si solo tenemos un fichero que tratar          
             job = queue.enqueue(CountingWords.contar_palabras_fichero,fichero)
             while job.result == None:
                 time.sleep(0.1)
             result = "El fichero tiene " + str(job.result) + " palabras\n"
             return result
-        else:
+        else:                                   #si tenemos mas de un fichero para tratar                
             jobResults = []
             for file in fichero.split():
                 workerResult= []
                 job = queue.enqueue(CountingWords.contar_palabras_fichero,file)
                 while job.result == None:
                     time.sleep(0.1)
-                workerResult.append(file)   #ponemos en el resultado del worker el nombre del fichero
+                workerResult.append(file)       #ponemos en el resultado del worker el nombre del fichero
                 workerResult.append(job.result) #ponemos en el resultado del worker el resultado obtenido
                 jobResults.append(workerResult) #añadimos este objeto a la lista
             job = queue.enqueue(putResultTogether.putResultTogether, args=(jobResults,"CountingWords"))
@@ -65,10 +65,10 @@ def CountingWordsServer(fichero):
                 time.sleep(0.1)
             result = str(job.result) + "\n"
             return result
-    else:
+    else:                                       #si no tenemos ningun worker creado
         return("No hi ha cap worker actiu per encuar la tasca")    
 
-
+#Inicializar worker
 def startWorker(id):
     listen = ['default']
 
@@ -81,6 +81,7 @@ def startWorker(id):
             worker = Worker(list(map(Queue, listen)))
             worker.work()
 
+#Crear worker
 def createWorker():
     global WORKERS
     global WORKER_ID
@@ -91,19 +92,21 @@ def createWorker():
     WORKER_ID+=1
     return (WORKER_ID-1)
 
+#Matar worker (pasandole su id)
 def killWorker(id):
     WORKERS[int(id)].terminate()
     return "Worker " + id + " eliminat correctament"
 
+#Numero de workers (devuelve un contador)
 def nWorkers():
     i = 0
-    result = ""
     for worker in WORKERS:
         poll = worker.is_alive()
         if poll is True:
             i+=1      
     return i
 
+#Listar workers y su estado
 def listWorkers():
     i = 0
     result = ""
@@ -116,9 +119,14 @@ def listWorkers():
         i+=1
     return result  
 
+#Workers
 WORKERS = []
 WORKER_ID = 0
+
+#Redis
 queue = Queue(connection=Redis())
+
+#Server
 server = SimpleXMLRPCServer(("localhost", 8000))
 print("Server is listening on port 8000...")
 server.register_function(createWorker, "createWorker")
